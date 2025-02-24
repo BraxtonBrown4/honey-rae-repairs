7a97b635edcc167800791832483b98b6e9fa493e
Braxton Brown <braxtoncarterbrown@gmail.com>
2025-02-24 11:54:37 -0600

    added user modules for reusable code, added user service to get users, and added customers page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { TicketList } from "./components/tickets/TicketList"
 import "./App.css"
+import { CustomerList } from "./components/customers/CustomerList"
 
 export const App = () => {
   return (
     <>
-      <TicketList/>
+      {/* <TicketList/> */}
+      <CustomerList/>
     </>
   )
 }

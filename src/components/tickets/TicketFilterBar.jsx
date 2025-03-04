16b95a6a75f6171e918dd65eeae42a20189c19e3
Braxton Brown <braxtoncarterbrown@gmail.com>
2025-03-04 14:23:55 -0600

    finsihed ticket form

--- src/components/tickets/TicketFilterBar.jsx
+++ src/components/tickets/TicketFilterBar.jsx
@@ -1,4 +1,8 @@
+import { useNavigate } from "react-router-dom"
+
 export const TicketFilterBar = ({ setSearchTerm, setShowEmergencyOnly, currentUser, setShowOpenOnly }) => {
+    const navigate = useNavigate()
+
     return (
         <div className="filter-bar">
             {
@@ -9,7 +13,7 @@ export const TicketFilterBar = ({ setSearchTerm, setShowEmergencyOnly, currentUs
                         <input type="text" placeholder="Search Tickets" className="ticket-search" onChange={(event) => { setSearchTerm(event.target.value) }} />
                     </> :
                     <>
-                        <button className="filter-btn btn-primary" onClick={() => {}}>Create Ticket</button>
+                        <button className="filter-btn btn-primary" onClick={() => {navigate('/tickets/create')}}>Create Ticket</button>
                         <button className="filter-btn btn-info" onClick={() => {setShowOpenOnly(true)}}>Open Tickets</button>
                         <button className="filter-btn btn-secondary" onClick={() => {setShowOpenOnly(false)}}>All My Tickets</button>
                     </>
